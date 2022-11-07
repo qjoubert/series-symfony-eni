5fc207a21e2dd2ab65dd0ef67a1fb5f3fed840eb
Quentin Joubert <quentinjoubert123@gmail.com>
2022-11-07 14:25:07 +0100

    module 8 fini

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,6 +20,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
+        $user->setRoles(['ROLE_USER']);
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
